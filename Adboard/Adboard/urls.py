aeaa21213a32985bfefe41609fe268f4b6e70926
SergeyK1983 <ssp-serg@yandex.ru>
2024-03-03 17:53:41 +0300

    gunicorn

--- Adboard/Adboard/urls.py
+++ Adboard/Adboard/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views
 
 
 from .views import index, page_not_found
@@ -13,9 +13,9 @@ urlpatterns = [
     path('ann/', include('announcement.urls')),
     path('cab/', include('cabinet.urls')),
     path('com/', include('coment.urls')),
-    path('auth/', views.obtain_auth_token, name='auth'),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    # path('auth/', views.obtain_auth_token, name='auth'),
+    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
 
 if settings.DEBUG:
---
+++ config/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/home/serg/code/BulletinBoard/venv/bin/gunicorn'
+pythonpath = '/home/serg/code/BulletinBoard/Adboard'
+bind = '127.0.0.1:8000'
+workers = 5
